be98e7ee14767e9c227300350cf7d62114906d8e
henrymcruz <henrymcruz04@gmail.com>
2025-05-12 09:55:58 -0300

    Estrutaração de dominio

--- src/main/java/br/com/henrymori/desafio_todolist/repository/TodoRepository.java
+++ src/main/java/br/com/henrymori/desafio_todolist/repository/TodoRepository.java
@@ -1,5 +1,6 @@
 package br.com.henrymori.desafio_todolist.repository;
 
+import br.com.henrymori.desafio_todolist.entity.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TodoRepository extends JpaRepository<Todo, Long>{
