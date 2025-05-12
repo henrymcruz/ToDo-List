e8ff3d619627793a5013b32d01f40e383c6129b0
henrymcruz <henrymcruz04@gmail.com>
2025-05-12 09:23:58 -0300

    Estruturação de projeto v2

--- src/main/java/br/com/henrymori/desafio_todolist/repository/TodoRepository.java
+++ src/main/java/br/com/henrymori/desafio_todolist/repository/TodoRepository.java
@@ -1,4 +1,7 @@
 package br.com.henrymori.desafio_todolist.repository;
 
-public interface TodoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoRepository extends JpaRepository<Todo, Long>{
+
 }
